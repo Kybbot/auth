bf5f806814f4db31830f74a8a491cf68f86917d2
Артём Одийко <kybbot1@gmail.com>
2021-09-27 15:44:27 +0300

    changed functions names

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -3,10 +3,10 @@ import { authApi } from './api';
 import { ILoginResponse } from '../types/login';
 import { ISignupResponse } from '../types/signup';
 
-export const signup = (body: string) => {
+export const signupWithEmailAndPassword = (body: string) => {
 	return authApi.post<ISignupResponse>('sign_up', body);
 };
 
-export const login = (email: string, password: string) => {
+export const loginWithEmailAndPassword = (email: string, password: string) => {
 	return authApi.post<ILoginResponse>(`login?email=${email}&password=${password}`);
 };
